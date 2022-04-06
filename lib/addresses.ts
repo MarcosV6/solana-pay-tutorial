19d3d053861326a79903846f5846138480093c5e
MarcosV6 <thisismarcos@gmail.com>
2022-04-06 14:28:56 -0400

    donuts

---
+++ lib/addresses.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js"
+
+// This is your shop address
+export const shopAddress = new PublicKey('D6oMDfqzcTuKu8tdpsvJbZ4VudaoR4LrToTbBTCmg3oe')
+// This is the same for everyone
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('4Gt497HKui8PWd7yDNYLKDZJH4kLBPf4sGnyAejbjKuH')
--- lib/products.ts
+++ lib/products.ts
@@ -2,17 +2,17 @@ export const products = [
   {
     id: 'box-of-cookies',
     name: 'Box',
-    description: 'A delicious box of handmade cookies',
+    description: 'A delicious box of handmade Donuts',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
-    priceSol: 0.05,
+    priceSol: 0.04,
     priceUsd: 5,
   },
   {
     id: 'basket-of-cookies',
-    name: 'Basket',
-    description: 'A large basket of handmade cookies',
+    name: 'Box Of Munchinkins',
+    description: 'A large box of Munchkins',
     unitName: 'basket',
     priceSol: 0.1,
-    priceUsd: 10,
+    priceUsd: 15,
   }
 ]

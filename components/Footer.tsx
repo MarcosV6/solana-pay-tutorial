9a46511a568641d0121cb3025ec7b9d56fe879fb
MarcosV6 <thisismarcos@gmail.com>
2022-04-07 15:14:29 -0400

    changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,11 @@ export default function Footer() {
   return (
     <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
       <a
-        href='https://www.pointer.gg?utm_source=solana-pay'
+        href='https://www.usingdefi.org'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Learn web3 dev and earn crypto rewards at{" "}
+        Learn how to us Defi at{" UsingDefi.org"}
         <span>Pointer</span>
       </a>
     </footer>
